7fe71de39aa6a23381123807107908ddb62b7a20
Dhanushkodi <dhanushkodir@piraiinfo.com>
2024-10-03 12:53:48 +0530

    deploymnet commit socket

---
+++ src/App.tsx
@@ -0,0 +1,47 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
+
+function App() {
+  const connectSocket = () => {
+    const token: {
+      transports: string[];
+      withCredentials: boolean;
+      secure: boolean;
+      reconnection: boolean;
+      auth?: { token: string | undefined };
+    } = {
+      transports: ["websocket"],
+      withCredentials: true,
+      secure: false,
+      reconnection: true,
+    };
+
+    const socket = io("http://10.10.11.72:9090", {
+      ...token,
+    });
+    console.log(socket);
+    return socket;
+  };
+
+  useEffect(() => {
+    connectSocket();
+  }, []);
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + WebSocket</h1>
+    </>
+  );
+}
+
+export default App;
